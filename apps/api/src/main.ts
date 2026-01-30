61d79391b899b20896f9252498a4b38bc40e3575
Nouman-Ali953 <nauman.9533@gmail.com>
2026-01-30 12:48:30 +0500

    husky checked

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,9 +7,6 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
-const a = 'foo';
-console.log(a);
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
