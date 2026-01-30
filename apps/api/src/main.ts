7ba282e887d7e64b38a459ba41e12f285e931a36
Nouman-Ali953 <nauman.9533@gmail.com>
2026-01-30 12:46:56 +0500

    checking husky

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,14 +7,18 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
-var a = 'foo';
+const a = 'foo';
+console.log(a);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
 
 bootstrap();
