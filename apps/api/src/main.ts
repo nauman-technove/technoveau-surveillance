5207bff1da3f2cbd4d745cf84cbb3d7bae6d468f
Nouman-Ali953 <nauman.9533@gmail.com>
2026-01-30 13:12:11 +0500

    added dependencies

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,9 +6,26 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-
+import { ZodValidationPipe } from '@anatine/zod-nestjs';
+import helmet from 'helmet';
+import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Security
+  app.use(helmet());
+
+  // Enable CORS only for localhost in development
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:4200'
+        : undefined,
+  });
+
+  // Validation
+  app.useGlobalPipes(new ZodValidationPipe());
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
